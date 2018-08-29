9f01ecd7476e02726ee6eb698721fe9b2f9d3c33
Brad Soper <brad.soper@gmail.com>
2018-08-28 18:20:04 -0700

    Updating default and circle with my provider code for a test

--- default.tf
+++ default.tf
@@ -4,9 +4,9 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_vpc" "minimal" {
-  cidr_block = "10.0.0.0/16"
-  tags {
-    Name = "minimal"
-  }
-}
+resource "aws_instance" "RadditwithFile" {
+  ami           = "ami-2757f631"
+  instance_type = "t2.micro"
+  count = 1
+  key_name   = "Default"
+  security_groups = ["launch-wizard-1"]
