916fa9787b2e48dcec5666623596224bb9a700eb
Brad Soper <brad.soper@gmail.com>
2018-08-29 08:49:45 -0700

    removing backend for testing

--- default.tf
+++ default.tf
@@ -1,11 +1,14 @@
 # Version 5
 provider "aws" {
   region = "us-east-1"
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
 }
 
-resource "aws_vpc" "minimal" {
-  cidr_block = "10.0.0.0/16"
-  tags {
-    Name = "minimal"
-  }
+resource "aws_instance" "RadditwithFile" {
+  ami           = "ami-2757f631"
+  instance_type = "t2.micro"
+  count = 1
+  key_name   = "Default"
+  security_groups = ["launch-wizard-1"]
 }
