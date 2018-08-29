d9cded45509565f932ad1485af476b353497d178
Brad Soper <brad.soper@gmail.com>
2018-08-29 11:53:32 -0700

    Added config and ubuntu service files and updated default to push those files.

--- default.tf
+++ default.tf
@@ -12,3 +12,40 @@ resource "aws_instance" "RadditwithFile" {
   key_name   = "Default"
   security_groups = ["launch-wizard-1"]
 }
+
+
+provisioner "file" {
+    source      = "configuration.sh"
+    destination = "/tmp/configuration.sh"
+    connection {
+        type = "ssh"
+        user = "ubuntu"
+        private_key = "${file("Default.pem")}"
+              }
+}
+
+provisioner "file" {
+    source      = "ubuntu.service"
+    destination = "/tmp/ubuntu.service"
+    connection {
+          type = "ssh"
+          user = "ubuntu"
+          private_key = "${file("Default.pem")}"
+                }
+}
+
+
+provisioner "remote-exec" {
+ inline = [
+   "chmod +x /tmp/configuration.sh",
+   "chmod +x /tmp/ubuntu.service",
+   "sudo /tmp/configuration.sh"
+          ]
+ connection {
+     type     = "ssh"
+     user     = "ubuntu"
+     private_key = "${file("Default.pem")}"
+           }
+           }
+
+}
